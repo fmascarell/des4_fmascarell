1b6473102baaa3bc5060e4829ad65fdc54fd940f
Francia Mascarell <fran.mascarell@hotmail.com>
2024-04-18 18:19:39 -0400

    update

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,1 +1,47 @@
 const socket = io();
+
+socket.on('productos', productos => {
+    console.log(productos);
+    const body = document.getElementById('productos-body');
+    body.innerHTML = '';
+
+    productos.array.forEach(producto => {
+        const row = body.insertRow();
+
+        row.innerHTML = `
+        <td>${producto.id}</td>
+        <td>${producto.title}</td>
+        <td>${producto.description}</td>
+        <td>${producto.price}</td>
+        <td>${producto.code}</td>
+        <td>${producto.stock}</td>
+        <td>${producto.category}</td>
+        <td>${producto.status ? 'Activo': 'Inactivo'}</td>
+        <td>${producto.thumbnail.lenght > 0 ? producto.thumbnail:'No hay imágen'}</td>
+        `;
+    });
+});
+
+const formulario = document.getElementById('form'); 
+
+formulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const titulo = document.getElementById('titulo').value;
+    const descripcion = document.getElementById('descripcion').value;
+    const precio = document.getElementById('precio').value;
+    const codigo = document.getElementById('codigo').value;
+    const stock = document.getElementById('stock').value;
+    const categoria = document.getElementById('categoria').value;
+
+    const producto = {
+        title: titulo,
+        description: descripcion,
+        price: precio,
+        code: codigo,
+        stock: stock,
+        category:categoria
+    }
+
+    socket.emit('addProduct', producto);
+    formulario.requestFullscreen();
+})
--- src/routers/app.js
+++ src/routers/app.js
@@ -6,10 +6,13 @@ import products from './routers/products.js';
 import carts from './routers/carts.js';
 import views from './routers/views.js';
 import __dirname from "./utils.js";
+import ProductManager from "./productManager.js";
 
 
 const app = express();
 const PORT = 8080;
+
+const prod = new ProductManager();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -32,5 +35,13 @@ const sockServer = new Server(expServer);
 
 sockServer.on('connection', socket => {
   console.log('Client connected from the front');
+  const productos = prod.getProducts();
+  socket.emit('productos', productos);
+
+  socket.on('addProduct', product =>{
+    const result = prod.addProduct(product);
+    console.log({result});
+  })
+
 });
 
